561b7587ecd72d4b8d2a05914340af08662b7de1
Diablohu <diablohudream@gmail.com>
2015-11-22 06:53:04 +0800

    videos

---
+++ !sources/js/base.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+	document.addEventListener('click', function(event) {
+		var links = document.querySelectorAll('a:not([target]):not([href^="/"]');
+		var target = event.target;
+		
+		for (var i = 0, l = links.length; i < l; i++) {
+			var el = target;
+			var p = links[i];
+			
+			while(el && el !== document) {
+				if (el === p) {
+					if( p.getAttribute('href').indexOf('//' + location.host) >= 0 ){
+						return p.setAttribute('target', '_self');
+					}else{
+						return p.setAttribute('target', '_blank');
+					}
+				}
+				
+				el = el.parentNode;
+			}
+		}
+	})
+})
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var babel = require("gulp-babel");
+
+gulp.task('default',[
+	'KanColleGuides'
+])
+
+
+
+
+
+
+
+
+// v2
+
+gulp.task('KanColleGuides-js', function(){
+	return gulp.src([
+			'./!sources/js/!.js',
+			
+			//'./node_modules/domready/src/ready.js',
+			
+			'./!sources/js/base.js'
+		])
+		.pipe(concat('js.js'))
+		.pipe(babel({
+			'highlightCode':	false,
+			'comments':			false,
+			'compact':			false,
+			'ast':				false
+		}))
+		//.pipe(uglify())
+		.pipe(gulp.dest('./assets'))
+})
+
+gulp.task('KanColleGuides-css', function(){
+	return gulp.src([
+			'./source/css-global.less'
+		])
+		.pipe(less())
+		.pipe(minifyCSS())
+		.pipe(postcss([
+			autoprefixer({
+				'browsers': [
+					'Android >= 2',
+					'Chrome >= 20',
+					'Firefox >= 20',
+					'ie >= 11',
+					'Edge >= 12',
+					'iOS >= 5',
+					'ChromeAndroid >= 20',
+					'ExplorerMobile >= 11'
+				]
+			})
+		]))
+		.pipe(gulp.dest('./assets'))
+})
+
+gulp.task('KanColleGuides-watch', function(){
+	gulp.watch(
+			'./!sources/**/*.js',
+			['KanColleGuides-js']
+		)
+	gulp.watch(
+			'./!sources/**/*.less',
+			['KanColleGuides-css']
+		)
+})
+
+gulp.task('KanColleGuides',[
+	'KanColleGuides-js',
+	'KanColleGuides-css',
+	'KanColleGuides-watch'
+])
