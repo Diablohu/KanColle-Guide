0fcd2739fc9b0172a32237cfba8ba344b6bd2b01
Diablohu <diablohudream@gmail.com>
2015-12-25 09:02:54 +0800

    update KCTip

--- !sources/js/base.js
+++ !sources/js/base.js
@@ -25,6 +25,38 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	})
 	*/
+
+	function delegationLinks(target){
+		if( target.getAttribute('href').indexOf('//' + location.host) >= 0 ){
+			return target.setAttribute('target', '_self');
+		}else{
+			return target.setAttribute('target', '_blank');
+		}
+	}
+	
+	function delegationVideoPlayerStart(target){
+		var body = target.offsetParent
+		body.innerHTML = body.querySelector('textarea').textContent
+	}
+
+	function handler_links(evt){
+		for (var target=evt.target; target && target!=this; target=target.parentNode) {
+			// loop parent nodes from the target to the delegation node
+			//if (target.matches('a:not([target]):not([href^="/"]')) {
+			if( elmatches(target, 'a:not([target]):not([href^="/"]):not([href^="javascript:"])') ){
+				return delegationLinks(target, evt);
+				break;
+			}else if( elmatches(target, '.videoplayer a.thumbnail') ){
+				return delegationVideoPlayerStart(target, evt);
+				break;
+			}
+		}
+	}
+
+	['click', 'pointerdown'].forEach(function(e){
+		document.addEventListener(e, handler_links)
+	})
+	/*
 	document.addEventListener("click", function(e) {
 		for (var target=e.target; target && target!=this; target=target.parentNode) {
 			// loop parent nodes from the target to the delegation node
@@ -38,17 +70,5 @@ document.addEventListener("DOMContentLoaded", function(){
 			}
 		}
 	}, false);
+	*/
 })
-
-function delegationLinks(target){
-	if( target.getAttribute('href').indexOf('//' + location.host) >= 0 ){
-		return target.setAttribute('target', '_self');
-	}else{
-		return target.setAttribute('target', '_blank');
-	}
-}
-
-function delegationVideoPlayerStart(target){
-	var body = target.offsetParent
-	body.innerHTML = body.querySelector('textarea').textContent
-}
