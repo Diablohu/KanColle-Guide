2e338ca179e8f3c25cdde6aa63d05c0e6ed68c6a
Diablohu <diablohudream@gmail.com>
2016-02-08 16:25:26 +0800

    update

--- assets/js.js
+++ assets/js.js
@@ -77,7 +77,45 @@ document.addEventListener("DOMContentLoaded", function () {
 			return html;
 		}
 
-		ticking(Date.parse(el.getAttribute('countdown')) - Date.parse(new Date()));
+		var endTimeString = el.getAttribute('countdown'),
+		    endTimeParse = Date.parse(endTimeString);
+		if (!endTimeParse || isNaN(endTimeParse)) {
+			(function () {
+				var parser = function parser(date) {
+					var timestamp,
+					    struct,
+					    minutesOffset = 0;
+
+					if (struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(date)) {
+						for (var i = 0, k; k = numericKeys[i]; ++i) {
+							struct[k] = +struct[k] || 0;
+						}
+
+						struct[2] = (+struct[2] || 1) - 1;
+						struct[3] = +struct[3] || 1;
+
+						if (struct[8] !== 'Z' && struct[9] !== undefined) {
+							minutesOffset = struct[10] * 60 + struct[11];
+
+							if (struct[9] === '+') {
+								minutesOffset = 0 - minutesOffset;
+							}
+						}
+
+						timestamp = Date.UTC(struct[1], struct[2], struct[3], struct[4], struct[5] + minutesOffset, struct[6], struct[7]);
+					} else {
+						timestamp = Date.parse ? Date.parse(date) : NaN;
+					}
+
+					return timestamp;
+				};
+
+				var numericKeys = [1, 4, 5, 6, 7, 10, 11];
+				;
+				endTimeParse = parser(endTimeString);
+			})();
+		}
+		ticking(endTimeParse - Date.parse(new Date()));
 	}
 	[].forEach.call(document.querySelectorAll('[countdown]'), countdown);
 });
